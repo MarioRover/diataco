68e6428ab25642687632e90f599afab4c56b06de
Hossein Akbari <mariorover021@gmail.com>
2018-08-20 14:18:54 +0430

    update rellax v1.2

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,19 @@
 //////////////Mobile Navigation/////////////////// 
 let mobileIcon = $('#nav-icon');
 let mobileNav = $('.mobile-nav');
+let headerSite = $('.header-site');
 $(document).ready(function () {
   $(mobileIcon).click(() => {
     if(!$(mobileIcon).hasClass('open')) {
       $(mobileIcon).addClass('open');
       $(mobileNav).addClass('open');
-      $('.homeSlider').css('margin-top', 0);
+      // $('.homeSlider').css('margin-top', 0);
+      $(headerSite).css('background-color', '#000');
     } else {
       $(mobileIcon).removeClass('open');
       $(mobileNav).removeClass('open');
-      $('.homeSlider').css('margin-top', '-7em');
+      // $('.homeSlider').css('margin-top', '-7em');
+      $(headerSite).css('background-color', '#bfd5e3');
     }
   });
 });
