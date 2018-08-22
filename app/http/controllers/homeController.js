f7deceded4d6290843e5b1c0e6300e2fdf326d30
Hossein Akbari <mariorover021@gmail.com>
2018-08-23 02:39:18 +0430

    update 404 page v1.03

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -4,9 +4,6 @@ class homeController extends controller {
   showPage(req , res) {
     res.render('home/home');
   }
-  page404(req , res) {
-    res.render('pageNotFound');
-  }
 }
 
 module.exports = new homeController();
--- app/routes/web/home.js
+++ app/routes/web/home.js
@@ -5,7 +5,5 @@ const router = express.Router();
 const homeController = require('app/http/controllers/homeController');
 // Home Routes
 router.get('/' , homeController.showPage);
-// Page not Found
-router.get('/404' , homeController.page404);
 
 module.exports = router;
